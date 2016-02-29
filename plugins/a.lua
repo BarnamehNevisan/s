047ad6538b82f46f12074b3333334b95d44fc734
es <barnameh.nevisan.2016@gmail.com>
2016-02-29 13:39:34 +0330

    Create a.lua

---
+++ plugins/a.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return [[
+/star
+   ]]
+
+  end
+return {
+  description = "spam only", 
+  usage = "/star : return spams",
+  patterns = {
+    "^/star$",
+
+  },
+  run = run
+}
+end
